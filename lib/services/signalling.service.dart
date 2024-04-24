665bee35bbaad5a448146967a9deefe4435564ec
Confidence Ugolo <ugoloconfidence@gmail.com>
2024-04-24 10:26:18 +0100

    Setup

---
+++ lib/services/signalling.service.dart
@@ -0,0 +1,32 @@
+import 'dart:developer';
+import 'package:socket_io_client/socket_io_client.dart';
+
+class SignallingService {
+  // instance of Socket
+  Socket? socket;
+
+  SignallingService._();
+  static final instance = SignallingService._();
+
+  init({required String websocketUrl, required String selfCallerID}) {
+    print("Setting up");
+    // init Socket
+    socket = io(websocketUrl, {
+      "transports": ['websocket'],
+      "query": {"callerId": selfCallerID}
+    });
+
+    // listen onConnect event
+    socket!.onConnect((data) {
+      print("Socket connected !!");
+    });
+
+    // listen onConnectError event
+    socket!.onConnectError((data) {
+      print("Connect Error $data");
+    });
+
+    // connect socket
+    socket!.connect();
+  }
+}
